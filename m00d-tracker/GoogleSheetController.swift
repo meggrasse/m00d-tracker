7f91c4d9e7048c8429fe3b77694a45d374ff1944
meggrasse <meggrasse@u.northwestern.edu>
2018-08-22 09:19:52 -0500

    Clean up GoogleSheetController.

--- m00d-tracker/GoogleSheetController.swift
+++ m00d-tracker/GoogleSheetController.swift
@@ -18,10 +18,7 @@ class GoogleSheetController: NSObject {
         self.spreadsheetID = sheetID
     }
     
-    func writeToSpreadsheetColumn(range: String, values: [String]) {
-        let sheetValues = GTLRSheets_ValueRange()
-        sheetValues.majorDimension = "COLUMNS"
-        sheetValues.values = [values]
+    func writeToSpreadsheet(range: String, sheetValues: GTLRSheets_ValueRange) {
         if let sheetID = spreadsheetID, let service = sheetsService {
             let sheetsQuery = GTLRSheetsQuery_SpreadsheetsValuesUpdate.query(withObject: sheetValues, spreadsheetId: sheetID, range: range)
             sheetsQuery.valueInputOption = "USER_ENTERED"
@@ -36,29 +33,40 @@ class GoogleSheetController: NSObject {
         }
     }
     
-    // Scans cells in the first row starting with the second column until it finds a blank, and returns in the column ID of that blank
-    func findFirstBlankColumn(completion: @escaping (_ ColumnID: String?)->()) {
+    func readFromSpreadsheet(range: String, readRows: Bool, completion: @escaping(_ values: [[Any]]?) -> ()) {
         if let sheetID = spreadsheetID, let service = sheetsService {
-            // hopefully it can handle this
-            // Search through all cells in the first row starting with the second column
-            let range = "Sheet1!B1:1"
             let sheetsQuery = GTLRSheetsQuery_SpreadsheetsValuesGet.query(withSpreadsheetId: sheetID, range: range)
-            sheetsQuery.majorDimension = "ROWS"
+            sheetsQuery.majorDimension = readRows ? "ROWS" : "COLUMNS"
             service.executeQuery(sheetsQuery, completionHandler: { (ticket: GTLRServiceTicket, result: Any?, error: Error?) in
                 guard let queryResult = result as? GTLRSheets_ValueRange else {
                     // something more elegant here?
                     print("We at an error")
                     return
                 }
-                let entryCount = queryResult.values?[0].count ?? 0
-                let blankColumnIndex = entryCount + 1
-                if let asciiInt = UnicodeScalar(blankColumnIndex + 65) {
-                    completion(String(asciiInt))
-                }
+                completion(queryResult.values)
             })
         } else {
             print("Something not configured correctly.")
         }
     }
-
+    
+    func writeToSpreadsheetColumn(range: String, values: [String]) {
+        let sheetValues = GTLRSheets_ValueRange()
+        sheetValues.majorDimension = "COLUMNS"
+        sheetValues.values = [values]
+        writeToSpreadsheet(range: range, sheetValues: sheetValues)
+    }
+    
+    // Scans cells in the first row starting with the second column until it finds a blank, and returns in the column ID of that blank
+    func findFirstBlankColumn(completion: @escaping (_ ColumnID: String?) -> ()) {
+        // Search through all cells in the first row starting with the second column
+        let range = "Sheet1!B1:1"
+        readFromSpreadsheet(range: range, readRows: true, completion: { (values) in
+            let entryCount = values?[0].count ?? 0
+            let blankColumnIndex = entryCount + 1
+            if let asciiInt = UnicodeScalar(blankColumnIndex + 65) {
+                completion(String(asciiInt))
+            }
+        })
+    }
 }
